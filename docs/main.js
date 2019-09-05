76cf1bbbddb1e2c563c4991e4b98de15e6074cc4
Zirui <fanzirui@yahoo.com>
2019-09-05 12:36:03 +0800

    Update

--- docs/main.js
+++ docs/main.js
@@ -3,16 +3,29 @@ let navHeightExpanded = "20.1rem";
 let navRightHeightFolded = "0";
 let navRightHeightExpanded = "16rem";
 
-
 let nav = document.querySelector("#nav");
 let navLeft = document.querySelector("#nav-left");
 let navRight = document.querySelector("#nav-right");
 let main = document.querySelector(".main");
+let copyright = document.querySelector("#copyright");
 
-function init() {
+function resizeMain() {
     let height = document.documentElement.clientHeight;
 
     main.style.height = height + "px";
+}
+
+function insertYear() {
+    let date = new Date;
+    let year = date.getFullYear();
+
+    copyright.innerHTML = "<p>&copy; " + year + " 清华附中朝阳学校2019届3班</p>";
+}
+
+function init() {
+    resizeMain();
+
+    insertYear();
 }
 
 function menuIsFolded() {
