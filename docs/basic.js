d061f3bb5561f5bdaf631064b7ddcdf8a6d7fe68
Zirui <fanzirui@yahoo.com>
2019-09-26 09:08:12 +0800

    封装了基本元素

--- docs/basic.js
+++ docs/basic.js
@@ -9,23 +9,51 @@ var navRight = document.querySelector("#nav-right");
 var main = document.querySelector(".main");
 var copyright = document.querySelector("#copyright");
 
-function resizeMain() {
-    var height = document.documentElement.clientHeight;
-
-    main.style.height = height - (4 + 2) * 16 + "px";
+function insertNav() {
+    nav.innerHTML = '<div id="nav-left">\
+            <button class="hidden" type="button">\
+                <img src="images/menu.svg" alt="menu">\
+            </button>\
+            \
+            <h1>\
+                <a href="index.html">直升机班的蹭饭地图</a>\
+            </h1>\
+            \
+            <button type="button" onclick="toggleMenu()">\
+                <img src="images/menu.svg" alt="menu">\
+            </button>\
+        </div>\
+        \
+        <div id="nav-right">\
+            <ul>\
+                <li class="tablet-border-bottom">\
+                    <a href="namelist.html">花名册</a>\
+                </li>\
+                <li class="tablet-border-bottom">\
+                    <a href="major.html">蹭专业</a>\
+                </li>\
+                <li class="tablet-border-bottom">\
+                    <a href="https://martaincreature.github.io/ClassWeekly/">往期周记</a>\
+                </li>\
+                <li>\
+                    <a href="https://github.com/MartainCreature/SpongingMap"><img src="images/github.svg"\
+                            alt="github"></img></a>\
+                </li>\
+            </ul>\
+        </div>';
 }
 
-function insertYear() {
+function insertCopyright() {
     var date = new Date;
     var year = date.getFullYear();
 
     copyright.innerHTML = "<p>&copy; " + year + " 范子睿</p>";
 }
 
-function init() {
-    resizeMain();
+function resizeMain() {
+    var height = document.documentElement.clientHeight;
 
-    insertYear();
+    main.style.height = height - (4 + 2) * 16 + "px";
 }
 
 function menuIsFolded() {
@@ -74,6 +102,14 @@ function updateMenu() {
         navRight.style.height = navRightHeightFolded;
         navRight.style.visibility = "hidden";
     }
+}
+
+window.onload = function() {
+    resizeMain();
+    
+    insertNav();
+
+    insertCopyright();
 }
 
 window.onresize = function() {
