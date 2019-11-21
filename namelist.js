3224c1d532c2319f0b35fa927c8685339734c49f
Zirui <fanzirui@yahoo.com>
2019-11-21 19:56:17 +0800

    Update

--- namelist.js
+++ namelist.js
@@ -1,5 +1,23 @@
-var namelist = document.querySelector("#namelist");
+var namelistList = new List(document.querySelector("#namelistUp"), document.querySelector("#namelistDown"));
 
-for (var i = 0; i < people.length; i++) {
-    addPeopleInfo(namelist, i, true, true, true, true);
+namelistList.quantity = 39;
+
+var namelist_peoplePart = document.querySelector("#peoplePart");
+
+namelistList.search = function(clss) {
+    namelist_peoplePart.innerHTML = "";
+
+    for (var i = 0; i < people.length && i < listCapasity; i++) {
+        addPeopleInfo(namelist_peoplePart, i + namelistList.startNum, true, true, true, true);
+    }
+}
+
+function onloadOther() {
+    getListCapasity();
+    
+    namelistList.show('');
+}
+
+function onresizeOther() {
+    getListCapasity();
 }
