263a4d8c2d9b4c428ed1f63d75c0d252fb4f994c
Zirui <fanzirui@yahoo.com>
2019-12-20 22:03:11 +0800

    Update & bug fixes

--- list.js
+++ list.js
@@ -5,28 +5,28 @@ function addPeopleInfo(container, i, schoolIncluded, majorIncluded, positionIncl
     <h3 id="name">' + people[i].name + '</h3>\
     </div>\
     </div>';
- 
+
     if (typeof (people[i].school) != "undefined" && schoolIncluded) {
         container.innerHTML = container.innerHTML.substring(0, container.innerHTML.length - 16) + '<p id="major">' + people[i].school + '</p>\
-        </div>\
-        </div>';
+    </div>\
+    </div>';
     }
 
     if (typeof (people[i].major) != "undefined" && majorIncluded) {
         container.innerHTML = container.innerHTML.substring(0, container.innerHTML.length - 16) + '<p id="major">' + people[i].major + '</p>\
-        </div>\
-        </div>';
+    </div>\
+    </div>';
     }
 
     if (typeof (people[i].position) != "undefined" && positionIncluded) {
         container.innerHTML = container.innerHTML.substring(0, container.innerHTML.length - 16) + '<p id="position">' + people[i].position + '</p>\
-        </div>\
-        </div>';
+    </div>\
+    </div>';
     }
 
     if (typeof (people[i].subject) != "undefined" && subjectIncluded) {
         container.innerHTML = container.innerHTML.substring(0, container.innerHTML.length - 16) + '<p id="subject">' + people[i].subject + '</p>\
-        </div>\
-        </div>';
+    </div>\
+    </div>';
     }
 }
